98dc1190c20c26f9392d0be881a8e628a080e6b7
XusanDev07 <133357417+XusanDev07@users.noreply.github.com>
2024-07-29 18:17:07 +0500

    nimeydi bu

--- bot/models/core.py
+++ bot/models/core.py
@@ -64,7 +64,6 @@ class Basket(models.Model):
             super().save(*args, **kwargs)
 
 
-
 class LastViewedProduct(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
